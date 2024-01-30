ed6e99f96875604ccb6d6ed8f888777a47462b31
vaibhavsraut <vaibhav.raut851@gmail.com>
2024-01-30 23:09:12 +0530

    UC1: User need to enter a valid First Email

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ text.addEventListener('input', function() {
     }
 });
 
+const email = document.querySelector('#email');
+const emailError = document.querySelector('.email-output');
+
+email.addEventListener('input', function() {
+    let emailRegex = /^[a-zA-Z]+([.+-]\w+)*@[a-zA-Z]+(\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?)$/;
+    if (emailRegex.test(email.value)) {
+        emailError.textContent = "";
+    } else {
+        emailError.textContent = "Email is Incorrect";
+    }
+});
+
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
 
